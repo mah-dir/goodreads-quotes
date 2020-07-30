bb233f3adf8a5c73a5af14fba0835f1fb481b662
Mahdir Sheikh <mahsheikhdir@gmail.com>
2020-07-30 03:14:31 -0700

    any urls work, quotes are now saved according to tag, book or author string that is in the url

--- index.js
+++ index.js
@@ -2,53 +2,102 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-const loadSite = async (url, pagination = 0) => {
-  if (pagination > 0) {
-    const pages = [];
-    for (let i = 0; i < pagination; i++) {
-      const pageURL = url + "?page=" + (i + 1);
-      const result = await axios(pageURL);
-      pages.push(cheerio.load(result.data));
-      console.log("Added page: " + pageURL);
-    }
-    return pages;
-  } else {
+const loadSite = async (url) => {
+  try {
     const result = await axios(url);
-    return [cheerio.load(result.data)];
+    return cheerio.load(result.data);
+  } catch (error) {
+    console.log("Problem loading the site...", error);
   }
 };
 
 const scrapeSite = async (url) => {
-  const pages = await loadSite(url, 10);
+  try {
+    // Load first page
+    const startPage = await loadSite(url);
+    // Grab the total number of pages
 
-  const quotes = [];
-  for (page in pages) {
-    const $ = pages[page];
-    $(".quoteText").map(function (i, el) {
-      let [quote] = $(this)
-        .text()
-        .match(/(?<=\“)(.*?)(?=\”)/g);
-      quotes.push(quote);
-    });
-    console.log("Scraped page: " + page + " of " + pages.length);
+    let pages = 1;
+
+    if (startPage(".next_page").length > 0) {
+      pages = Number(startPage(".next_page").prev().text());
+      console.log("There are " + pages + " pages for this URL: " + url);
+    }
+    // Scrape each page before going to the next
+
+    const quoteArray = [];
+
+    for (let i = 0; i < pages; i++) {
+      let currentURL = url + "?page=" + (i + 1);
+      console.log("Scraping ", currentURL);
+      let $ = await loadSite(currentURL);
+
+      $(".quoteDetails").each(function (i, el) {
+        quoteArray.push({
+          author: $(this)
+            .find("span.authorOrTitle")
+            .text()
+            .trim()
+            .replace(/,\s*$/, ""),
+          quote: $(this)
+            .find(".quoteText")
+            .text()
+            .match(/(?<=\“)(.*?)(?=\”)/g),
+          book: $(this).find("a.authorOrTitle").text().trim(),
+          likes: Number($(this).find(".right").text().trim().match(/\d+/g)),
+          tags: $(this)
+            .find(".left")
+            .contents()
+            .map(function (i, el) {
+              let p = $(this).text().trim();
+              if (i != 0 && p != "" && p != ",") {
+                return $(this).text().trim();
+              }
+            })
+            .get(),
+        });
+      });
+    }
+
+    return quoteArray;
+  } catch (error) {
+    console.log("Something went wrong...", error);
   }
-
-  const name = url.match(/([A-Z])\w+/g);
-  return [name, quotes];
 };
 
 (async () => {
-  const authorURLs = [
-    "https://www.goodreads.com/author/quotes/4918776.Seneca",
+
+  const URLs = [
+    "https://www.goodreads.com/work/quotes/969064-the-essential-groucho-writings-by-and-for-groucho-marx",
+    "https://www.goodreads.com/quotes/tag/animals"
   ];
 
-  allQuotes = {};
+  var dir = "./output";
 
-  for (author in authorURLs) {
-    const result = await scrapeSite(authorURLs[author]);
-    allQuotes[result[0]] = result[1];
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
   }
-  fs.writeFile("./output/quotes.json", JSON.stringify(allQuotes, null, "\t"), () => {
-    console.log("Quotes retrieved and recorded!");
-  });
+
+  let allQuotes = [];
+
+  for (u in URLs) {
+    const fileName = URLs[u].split("/").pop() + ".json";
+
+    if (fs.existsSync(dir + "/" + fileName)) {
+      console.log('Already scraped. File name is : ' + fileName);
+      continue;
+    }
+
+    const result = await scrapeSite(URLs[u]);
+    allQuotes = result;
+
+    fs.writeFile(
+      "./output/" + fileName,
+      JSON.stringify(allQuotes, null, "\t"),
+      () => {
+        console.log("Quotes retrieved and recorded! Can be found in file named: " + fileName);
+      }
+    );
+
+  }
 })();
